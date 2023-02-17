83c34fc454a18157896ae6c8274bd8077e8384c2
elie dimitri <dimi972@yahoo.fr>
2023-02-16 23:25:48 -0400

    finition du site

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,32 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+//= require jquery3
+//= require popper
+//= require bootstrap
 import "bootstrap"
+
+//javascript navbar
+$(document).ready(function() {
+  $(".navbar").hide().fadeIn(1000).slideDown(1000);
+});
+
+
+
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', () => {
+    const accordionContent = header.nextElementSibling;
+    if (accordionContent.style.display === 'block') {
+      accordionContent.style.display = 'none';
+    } else {
+      accordionContent.style.display = 'block';
+    }
+  });
+});
+$(document).ready(function(){
+  $('.accordion-header').click(function(){
+    $(this).find('.fa-caret-down').toggleClass('rotate');
+  });
+});
